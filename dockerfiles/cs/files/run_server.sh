744b5cd4a60e5651594cf99c7a3194813a3c6bb3
Paal Braathen <paalbra@gmail.com>
2021-03-16 22:16:40 +0100

    Add map as parameter

--- dockerfiles/cs/files/run_server.sh
+++ dockerfiles/cs/files/run_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ -z "$1" ];then
+    MAP="de_dust"
+else
+    MAP="$1"
+fi
+
 cd "${HOME}/cs/"
-./hlds_run -game cstrike +maxplayers 24 +map de_dust
+./hlds_run -game cstrike +maxplayers 24 +map "$MAP"
