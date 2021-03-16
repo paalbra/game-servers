f184baea33b58c69f03967544455a82900bb9fcc
Paal Braathen <paalbra@gmail.com>
2021-03-16 23:21:47 +0100

    Add game mode parameter

--- dockerfiles/cs/files/run_server.sh
+++ dockerfiles/cs/files/run_server.sh
@@ -6,5 +6,21 @@ else
     MAP="$1"
 fi
 
+if [ -z "$2" ];then
+    MODE="normal"
+else
+    MODE="$2"
+fi
+
+if [ "$MODE" = "normal" ]; then
+    sed -i 's/pb_autokill .*/pb_autokill 1/g' cs/cstrike/addons/podbot/podbot.cfg
+    sed -i 's/mp_timelimit .*/mp_timelimit 20/g' cs/cstrike/server.cfg
+    sed -i 's/gungame.amxx/;gungame.amxx/g' cs/cstrike/addons/amxmodx/configs/plugins.ini
+elif [ "$MODE" = "gungame" ]; then
+    sed -i 's/pb_autokill .*/pb_autokill 0/g' cs/cstrike/addons/podbot/podbot.cfg
+    sed -i 's/mp_timelimit .*/mp_timelimit 0/g' cs/cstrike/server.cfg
+    sed -i 's/;gungame.amxx/gungame.amxx/g' cs/cstrike/addons/amxmodx/configs/plugins.ini
+fi
+
 cd "${HOME}/cs/"
 ./hlds_run -game cstrike +maxplayers 24 +map "$MAP"
