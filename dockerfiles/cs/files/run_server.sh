2a9307bc2ed709bf1f8e4371ece1e13ef4e8dac7
Paal Braathen <paalbra@gmail.com>
2021-03-25 23:53:03 +0100

    Only comment once

--- dockerfiles/cs/files/run_server.sh
+++ dockerfiles/cs/files/run_server.sh
@@ -15,7 +15,7 @@ fi
 if [ "$MODE" = "normal" ]; then
     sed -i 's/pb_autokill .*/pb_autokill 1/g' cs/cstrike/addons/podbot/podbot.cfg
     sed -i 's/mp_timelimit .*/mp_timelimit 20/g' cs/cstrike/server.cfg
-    sed -i 's/gungame.amxx/;gungame.amxx/g' cs/cstrike/addons/amxmodx/configs/plugins.ini
+    sed -i 's/^gungame.amxx/;gungame.amxx/g' cs/cstrike/addons/amxmodx/configs/plugins.ini
 elif [ "$MODE" = "gungame" ]; then
     sed -i 's/pb_autokill .*/pb_autokill 0/g' cs/cstrike/addons/podbot/podbot.cfg
     sed -i 's/mp_timelimit .*/mp_timelimit 0/g' cs/cstrike/server.cfg
